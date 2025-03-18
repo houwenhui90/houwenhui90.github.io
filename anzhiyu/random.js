f7fc83cd24122b3e6646f71fde7af2d029e8c734
houwenhui90 <houwenhui90@gmail.com>
2025-03-18 20:46:15 +0800

    Site updated: 2025-03-18 20:46:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/18/mark/","2025/03/17/hello-world/"];function toRandomPost(){
+var posts=["2025/03/17/hello-world/","2025/03/18/mark/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
